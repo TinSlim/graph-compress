7d5dd1b68cd36ec5d6ab4b196103ce8d9d8d1db5
Cristóbal Torres <44932278+TinSlim@users.noreply.github.com>
2023-12-03 00:00:26 -0300

    feat: Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="graphpress",
+    version="0.1.0",
+    description="graphpress is a Python library designed to compress graphs using arrays ",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
